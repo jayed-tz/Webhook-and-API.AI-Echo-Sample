bfa00c4d6f436bb95a8fb08853f08c26f5855fab
Jayed <jayed.talukder@orbitax.com>
2017-08-20 16:36:02 +0600

    json str

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again.";
     var sessionId=req.body.result.parameters.sessionId?req.body.result.parameters.sessionId:"no session id";
     return res.json({
-        speech: speech+" JAYED"+sessionId,
+        speech: speech+" JAYED"+sessionId+ JSON.stringify(req.body.result),
         displayText: speech+" JAYED"+sessionId,
         source: 'webhook-echo-sample'
     });
