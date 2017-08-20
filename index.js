79cf1b229bf019359e47275af4c79fcf62eb1b51
Jayed <jayed.talukder@orbitax.com>
2017-08-20 18:36:11 +0600

    tst

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ restService.use(bodyParser.json());
 restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again.";
 
+    if(!req.body.result)
+    {
+        return res.json({
+            speech: speech+" JAYED"+JSON.stringify(req.body),
+            displayText: speech,
+            source: 'webhook-echo-sample'
+        });
+    }
 
     if(req.body.result.metadata.intentName=="WelcomeIntent")
     {
