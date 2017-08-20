f7dbe7cab20825de2e5ab011d8c417f28002f267
Jayed <jayed.talukder@orbitax.com>
2017-08-20 16:28:12 +0600

    session id

--- index.js
+++ index.js
@@ -12,10 +12,11 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
-    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again.";
+    var sessionId=req.body.result.parameters.sessionId?req.body.result.parameters.sessionId:"no session id";
     return res.json({
-        speech: speech+" JAYED",
-        displayText: speech+" JAYED",
+        speech: speech+" JAYED"+sessionId,
+        displayText: speech+" JAYED"+sessionId,
         source: 'webhook-echo-sample'
     });
 });
