bd5cbc6ae2e945b4c08778b8c6b660794bfc2a83
Jayed <jayed.talukder@orbitax.com>
2017-08-20 16:55:01 +0600

    welcome intent

--- index.js
+++ index.js
@@ -13,10 +13,21 @@ restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again.";
-    var sessionId=req.body.result.parameters.sessionId?req.body.result.parameters.sessionId:"no session id";
+
+
+    if(req.body.result.metadata.intentName=="WelcomeIntent")
+    {
+        return res.json({
+            speech: "Hello "+req.body.sessionId,
+            displayText:  "Hello "+req.body.sessionId,
+            source: 'webhook-echo-sample'
+        });
+    }
+
+
     return res.json({
         speech: speech+" JAYED"+req.body.sessionId+ JSON.stringify(req.body),
-        displayText: speech+" JAYED"+sessionId,
+        displayText: speech,
         source: 'webhook-echo-sample'
     });
 });
